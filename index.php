f4e8942c84b68a2a1954f489b5876f04fc14e4a5
davide <davide.baiocchi97@gmail.com>
2021-07-27 13:07:08 +0200

    init

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google Faq</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <header>
+        <img src="https://1000marche.net/wp-content/uploads/2020/03/Google-logo.png" alt="">
+        <span>Privacy e termini</span>
+
+        <nav>
+            
+        </nav>
+    </header>
+    
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+
+</body>
+</html>
